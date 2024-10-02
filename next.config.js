e591dfd38dbc73527cdd83a2a0dad8b7f3eeb258
blocklinklabs <blocklinklabs@gmail.com>
2024-10-02 09:08:03 +0000

    http3

--- next.config.js
+++ next.config.js
@@ -9,3 +9,20 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 });
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
